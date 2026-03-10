aad996ae5d317b1e82ea3acb07fc74b8cf801fbe
Anas00123 <mohdanas2253@gmail.com>
2026-03-10 14:47:19 +0530

    fix application file

---
+++ assets/js/api.js
@@ -0,0 +1,29 @@
+const BASE_URL = "https://malcom-isopentyl-malvina.ngrok-free.dev";
+
+async function apiFetch(endpoint, options = {}) {
+
+    const response = await fetch(`${BASE_URL}${endpoint}`, {
+        method: options.method || "GET",
+        credentials: "include",
+        headers: {
+            "Content-Type": "application/json",
+            "ngrok-skip-browser-warning": "true",
+            ...(options.headers || {})
+        },
+        body: options.body ? JSON.stringify(options.body) : undefined
+    });
+
+    // If session expired
+    if (response.status === 401 || response.status === 403) {
+        alert("Session expired. Please login again.");
+        window.location.href = "login.html";
+        return;
+    }
+
+    if (!response.ok) {
+        const text = await response.text();
+        throw new Error(text || "API request failed");
+    }
+
+    return response.json();
+}s
